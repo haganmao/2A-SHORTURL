b3b0285b5bf02fd61868aff3be1b81fb56ef2681
haganmao <kevin_mmc@hotmail.com>
2020-04-18 00:57:39 +1200

    update check box of html

--- view/home.py
+++ view/home.py
@@ -6,6 +6,9 @@ from tornado.web import RequestHandler as requesthandler
 #Home handler, rendering html pages
 class HomeHandler(requesthandler):
      def get (self):
-         self.render("home.html")
+         data = dict(
+             cardTitle = "Try 2a Short Url Provider, You'll Like It"
+         )
+         self.render("home.html", data=data)
 
 
